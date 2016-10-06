f0b1a9e3a21fe5575a83f192670331a2ce68a32f
Juracy Filho <juracy@gmail.com>
2016-10-05 22:58:56 -0300

    Flatting src, index.js and index.ts at same dir

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
 "use strict";
-var index_1 = require('./src/index');
-exports.SweetAlertService = index_1.SweetAlertService;
+var sweetAlert = require('sweetalert2');
+var SweetAlertService = (function () {
+    function SweetAlertService() {
+    }
+    SweetAlertService.prototype.swal = function (args) {
+        if (args === void 0) { args = {}; }
+        return sweetAlert(args);
+    };
+    SweetAlertService.prototype.prompt = function (options) {
+        var baseOptions = {
+            showCancelButton: true,
+            confirmButtonText: 'Submit',
+            input: 'text'
+        };
+        return sweetAlert(Object.assign({}, baseOptions, options));
+    };
+    SweetAlertService.prototype.confirm = function (options) {
+        var baseOptions = {
+            showCancelButton: true,
+            confirmButtonText: 'Confirm',
+            type: 'warning'
+        };
+        return sweetAlert(Object.assign(baseOptions, options));
+    };
+    SweetAlertService.prototype.alert = function (options) {
+        var baseOptions = {
+            confirmButtonText: 'OK',
+            type: 'info'
+        };
+        return sweetAlert(Object.assign(baseOptions, options));
+    };
+    SweetAlertService.prototype.question = function (options) {
+        return this.alert(Object.assign({ type: 'question' }, options));
+    };
+    SweetAlertService.prototype.success = function (options) {
+        return this.alert(Object.assign({ type: 'success' }, options));
+    };
+    SweetAlertService.prototype.error = function (options) {
+        return this.alert(Object.assign({ type: 'error' }, options));
+    };
+    SweetAlertService.prototype.warn = function (options) {
+        return this.alert(Object.assign({ type: 'warn' }, options));
+    };
+    SweetAlertService.prototype.info = function (options) {
+        return this.alert(Object.assign({ type: 'info' }, options));
+    };
+    return SweetAlertService;
+}());
+exports.SweetAlertService = SweetAlertService;
--- index.ts
+++ index.ts
@@ -1,1 +1,55 @@
-export { SweetAlertService } from './src/index';
+var sweetAlert = require('sweetalert2');
+
+export class SweetAlertService {
+  constructor() {}
+
+  swal(args = {}) {
+    return sweetAlert(args);
+  }
+
+  prompt(options) {
+    const baseOptions = {
+      showCancelButton: true,
+      confirmButtonText: 'Submit',
+      input: 'text'
+    };
+    return sweetAlert(Object.assign({}, baseOptions, options));
+  }
+
+  confirm(options) {
+    const baseOptions = {
+      showCancelButton: true,
+      confirmButtonText: 'Confirm',
+      type: 'warning'
+    };
+    return sweetAlert(Object.assign(baseOptions, options));
+  }
+
+  alert(options) {
+    const baseOptions = {
+      confirmButtonText: 'OK',
+      type: 'info'
+    };
+    return sweetAlert(Object.assign(baseOptions, options));
+  }
+
+  question(options) {
+    return this.alert(Object.assign({ type: 'question' }, options));
+  }
+
+  success(options) {
+    return this.alert(Object.assign({ type: 'success' }, options));
+  }
+
+  error(options) {
+    return this.alert(Object.assign({ type: 'error' }, options));
+  }
+
+  warn(options) {
+    return this.alert(Object.assign({ type: 'warn' }, options));
+  }
+
+  info(options) {
+    return this.alert(Object.assign({ type: 'info' }, options));
+  }
+}
